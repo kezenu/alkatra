0f1a24e42501b1be0ba96db52870f0c92762a19e
Wisnu Sanjaya <wsanjaya69@gmail.com>
2025-04-15 13:39:00 +0700

    revisi bagian footer dan penggabungan bagian tenatng. ke index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,13 @@
 fetch("components/nav.html")
     .then(response => response.text())
     .then(data => document.getElementById("navbar-container").innerHTML = data);
+
+
+// Mmuat Bagian Tentang
+fetch ("components/tentang.html")
+    .then(response => response.text())
+    .then(data => document.getElementById("tentang").innerHTML = data )
+
 // memuat footer
 fetch("components/footer.html")
     .then(response => response.text())
